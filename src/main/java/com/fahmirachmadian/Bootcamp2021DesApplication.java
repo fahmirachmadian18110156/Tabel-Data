50c5323d2e781653f6c7620be1dcbd6a12449e35
Fahmi Rachmadian S <Fahmirachmadian18110156@gmail.com>
2021-12-14 15:06:06 +0700

    New Project

---
+++ src/main/java/com/fahmirachmadian/Bootcamp2021DesApplication.java
@@ -0,0 +1,13 @@
+package com.fahmirachmadian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootcamp2021DesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bootcamp2021DesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fahmirachmadian/Bootcamp2021DesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fahmirachmadian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bootcamp2021DesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
